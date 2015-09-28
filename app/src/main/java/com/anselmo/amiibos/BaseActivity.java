31a2a5ac3c837b4f45238d38cdbba3a36e56a3cd
Anselmo Hernandez <anselmo.hernandez@naranya.com>
2015-09-28 14:37:59 -0500

    Initial commit

---
+++ app/src/main/java/com/anselmo/amiibos/BaseActivity.java
@@ -0,0 +1,53 @@
+package com.anselmo.amiibos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by Anselmo Hernandez on 9/28/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        getToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected Toolbar getToolbar() {
+        if (mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.toolbar);
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+                Log.i("TOOLBAR_STATUS", "Se inicializo correctamente el Toolbar");
+            }
+        }
+        return mToolbar;
+    }
+}
---
+++ app/src/main/java/com/anselmo/amiibos/MainActivity.java
@@ -0,0 +1,12 @@
+package com.anselmo.amiibos;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
